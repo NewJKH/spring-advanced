7fc9ffa7d914e8a27ccfe390afef00f321c4b511
NewJKH <newsun0805@gmail.com>
2025-06-09 12:31:23 +0900

    refactor: "Lv3-1 ) param 위치 변경"

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -11,8 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class PasswordEncoderTest {
 
     @InjectMocks
-    private PasswordEncoder passwordEncoder;
-
+    PasswordEncoder passwordEncoder;
     @Test
     void matches_메서드가_정상적으로_동작한다() {
         // given
@@ -20,7 +19,7 @@ class PasswordEncoderTest {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
