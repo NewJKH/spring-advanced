48701f3d95e0e272658eed588c305f56c01b4d97
NewJKH <newsun0805@gmail.com>
2025-06-09 16:42:52 +0900

    feat: "LoggerInterceptor 인터셉터 등록"

--- src/main/java/org/example/expert/config/WebConfig.java
+++ src/main/java/org/example/expert/config/WebConfig.java
@@ -1,8 +1,11 @@
 package org.example.expert.config;
 
 import lombok.RequiredArgsConstructor;
+import org.example.expert.config.logger.LoggerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,9 +14,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private LoggerInterceptor loggerInterceptor;
+
     // ArgumentResolver 등록
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthUserArgumentResolver());
+    }
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(loggerInterceptor).addPathPatterns("/admin/**");
     }
 }
