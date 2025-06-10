902138c6e4fb3cca8c62cb0601281d6a3d0241dc
NewJKH <newsun0805@gmail.com>
2025-06-10 19:14:05 +0900

    feat: "Lv5 ) 예외처리 메세지 추가 ( @Valid )"

--- src/main/java/org/example/expert/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/expert/config/GlobalExceptionHandler.java
@@ -5,11 +5,15 @@ import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -32,6 +36,23 @@ public class GlobalExceptionHandler {
         return getErrorResponse(status, ex.getMessage());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> handleValidException(MethodArgumentNotValidException ex){
+        HttpStatus status = HttpStatus.resolve(ex.getStatusCode().value());
+        Map<String, String> fieldErrors = new LinkedHashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            fieldErrors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        Map<String, Object> errorResponse = new LinkedHashMap<>();
+        errorResponse.put("status", Objects.requireNonNull(status).name());
+        errorResponse.put("code", status.value());
+        errorResponse.put("message", "입력값이 올바르지 않습니다.");
+        errorResponse.put("errors", fieldErrors);
+
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
     public ResponseEntity<Map<String, Object>> getErrorResponse(HttpStatus status, String message) {
         Map<String, Object> errorResponse = new HashMap<>();
         errorResponse.put("status", status.name());
@@ -40,5 +61,6 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorResponse, status);
     }
+
 }
 
